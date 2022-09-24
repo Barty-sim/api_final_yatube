f6d8c71712badfd7de41fb947ffa8d1055f5b98d
Сергей Терещенко <barty.sim1@yandex.ru>
2022-09-24 13:11:32 +0300

    kl

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# api_final
-api final
+### Описание проекта
+**Это API создан для взаимодействия с прошлым проектом Yatube и построен на его базе**
+### Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+```
+https://github.com/Artem020793/api_final_yatube
+```
+```
+cd api_final_yatube
+```
+Cоздать и активировать виртуальное окружение:
+```
+python -m venv venv
+```
+```
+. venv/Scripts/activate
+```
+```
+python -m pip install --upgrade pip
+```
+Установить зависимости из файла requirements.txt:
+```
+pip install -r requirements.txt
+```
+Выполнить миграции:
+```
+python manage.py migrate
+```
+Запустить проект:
+```
+python manage.py runserver
+```
+### Примеры
+**Для доступа к API необходимо получить токен: 
+Нужно выполнить POST-запрос localhost:8000/api/v1/token/ передав поля username и password. API вернет JWT-токен**
+**Дальше, передав токен можно будет обращаться к методам, например: **
+**/api/v1/posts/ (GET, POST, PUT, PATCH, DELETE)**
+**При отправке запроса передавайте токен в заголовке Authorization: Bearer <токен>**
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,62 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import permissions, viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+
+from posts.models import Group, Post, User
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (CommentSerializer,
+                          GroupSerializer,
+                          PostSerializer,
+                          FollowSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
+    ]
+    pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('group',)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+
+class FollowViewSet(CreateModelMixin, ListModelMixin,
+                    viewsets.GenericViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('user__username', 'following__username')
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user.username)
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
